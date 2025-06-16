c98294f68e93ae01213251bcf9aaf5ea7815080b
Marios Sofokleous <marios.sofokleous@yandex.com>
2025-06-16 14:53:50 +0300

    Migrate build system from Gulp.js to Vite
    - Replace Fancybox with basicLightbox, removing jQuery dependency
    - Update all packages to latest versions
    - Enable individual JavaScript plugin imports for optimized bundle size

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-Front-end Trial
-====
+# Front-end Trial
 
 Static implementation of a website mockup. A take-home project, part of the recruitment process for a Front-end developer position at mtc.
 
-## Setup
+## Development Setup
 
-Simply fork the repo and run ```npm install``` in order to get all the Gulp dev dependencies. Next, run ```gulp watch``` to build the project and start live reloading on localhost:3000. At this point, the page is ready to go and you can begin to add your own information and make any needed changes.
+```
+npm install
+npm run dev
+```
 
 ## License
 
---
+++ src/js/main.js
@@ -0,0 +1,64 @@
+
+// Import Tiny Slider
+import { tns } from 'tiny-slider/src/tiny-slider.js';
+import 'tiny-slider/dist/tiny-slider.css';
+
+// Import our custom CSS
+import '../scss/styles.scss';
+// Import specific Bootstrap plugins
+import { Dropdown, Collapse } from 'bootstrap';
+
+// Import AOS (Animate On Scroll) library for animations
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+// Import Basic Lightbox for image lightbox functionality
+import 'basiclightbox/dist/basicLightbox.min.css';
+import * as basicLightbox from 'basiclightbox';
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  // Initialize Tiny Slider
+  tns({
+    container: '.testimonials-slider',
+    items: 1,
+    autoplay: false,
+    navPosition: "bottom",
+    controlsPosition: "bottom",
+    controlsText: ['<svg xmlns="http://www.w3.org/2000/svg" width="70.711" height="70.711" viewBox="0 0 70.711 70.711"><path d="M0,49V1A1,1,0,0,1,1,0H49a1,1,0,1,1,0,2H2V49a1,1,0,1,1-2,0Z" transform="translate(0 35.356) rotate(-45)" fill="#d4b254"/></svg>', '<svg xmlns="http://www.w3.org/2000/svg" width="70.711" height="70.711" viewBox="0 0 70.711 70.711"><path d="M0,49V1A1,1,0,0,0-1,0H-49a1,1,0,0,0-1,1,1,1,0,0,0,1,1H-2V49a1,1,0,0,0,1,1A1,1,0,0,0,0,49Z" transform="translate(70.711 35.356) rotate(45)" fill="#d4b254"/></svg>']
+  });
+
+  // Add/remove background to navbar
+  const primaryMenu = document.getElementById('primaryMenu')
+  const customNavbar = document.querySelector('.custom-navbar');
+
+  primaryMenu.addEventListener('show.bs.collapse', function () {
+    customNavbar.classList.add('custom-navbar_bg');
+  })
+  primaryMenu.addEventListener('hidden.bs.collapse', function () {
+    customNavbar.classList.remove('custom-navbar_bg');
+  })
+
+  // Initialize AOS
+  AOS.init({ once: true });
+
+  document.querySelectorAll('a[data-basiclightbox="gallery"]').forEach(link => {
+    link.addEventListener('click', event => {
+      event.preventDefault() // Prevent default link behavior
+
+      const imgSrc = link.getAttribute('data-src')
+      const caption = link.getAttribute('data-caption')
+
+      // Build the lightbox content
+      let content = `
+        <figure class="lightbox">
+          <img src="${imgSrc}" class="lightbox__image" alt="${caption}">
+          <figcaption class="lightbox__caption">${caption}</figcaption>
+        </figure>
+      `;
+
+      // Show the lightbox
+      basicLightbox.create(content).show();
+    })
+  })
+});
